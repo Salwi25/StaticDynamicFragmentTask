089c5149099b8769d34c8b9d11d160fda0ba7f48
Salwa Aurellia <salwaaurellianuza@gmail.com>
2023-02-26 09:26:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/staticdynamicfragmenttask/SimpleFragment.java
@@ -0,0 +1,41 @@
+package com.example.staticdynamicfragmenttask;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SimpleFragment extends Fragment {
+
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
